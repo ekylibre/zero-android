db040c64854a61adf19bd6d204e23b07756f257a
Antoine Thiberville <antoinethib@hotmail.com>
2016-03-29 17:11:37 +0200

    issues are now saved locally with the button save

---
+++ app/src/main/java/ekylibre/api/Issue.java
@@ -0,0 +1,27 @@
+package ekylibre.api;
+
+import ekylibre.exceptions.HTTPException;
+
+import java.io.IOException;
+
+import org.apache.http.client.ClientProtocolException;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Created by antoine on 29/03/16.
+ */
+public class Issue {
+    public static long create(Instance instance, JSONObject attributes) throws JSONException, ClientProtocolException, IOException, HTTPException {
+
+        // JSONObject params = Instance.BundleToJSON(attributes);
+        JSONObject params = attributes;
+
+        JSONObject json = instance.post("/api/v1/issues", params);
+
+        long id = json.getLong("id");
+
+        return id;
+    }
+}
--- app/src/main/java/ekylibre/zero/provider/DatabaseHelper.java
+++ app/src/main/java/ekylibre/zero/provider/DatabaseHelper.java
@@ -6,7 +6,7 @@ import android.database.sqlite.SQLiteDatabase.CursorFactory;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
-    public static final int DATABASE_VERSION = 1;
+    public static final int DATABASE_VERSION = 2;
     public static final String DATABASE_NAME = "zero";
 
 		// Constructor
@@ -34,8 +34,16 @@ public class DatabaseHelper extends SQLiteOpenHelper {
                              + ", " + TrackingContract.CrumbsColumns.SYNCED + " INTEGER NOT NULL DEFAULT 0"
                              + ", " + TrackingContract.CrumbsColumns.METADATA + " TEXT"
                              + ")");
-            // case 1:
-            //  		database.execSQL("ALTER TABLE crumbs SET ..."); 
+            	case 1:
+            			database.execSQL("CREATE TABLE IF NOT EXISTS issues ("
+                             + IssueContract.IssueColumns._ID + " INTEGER PRIMARY KEY AUTOINCREMENT"
+                             + ", " + IssueContract.IssueColumns.NATURE + " VARCHAR(32) NOT NULL"
+                             + ", " + IssueContract.IssueColumns.SEVERITY + " INTEGER NOT NULL DEFAULT 3"
+                             + ", " + IssueContract.IssueColumns.EMERGENCY + " INTEGER NOT NULL DEFAULT 3"
+                             + ", " + IssueContract.IssueColumns.DESCRIPTION + " INTEGER NOT NULL DEFAULT 3"
+                             + ", " + IssueContract.IssueColumns.PINNED + " BOOLEAN NOT NULL DEFAULT 0"
+                             + ", " + IssueContract.IssueColumns.SYNCED_AT + " DATE"
+                             + ")");
 				}
 		}
 
