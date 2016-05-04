5367ba3ecd5d89142507102c93bcb764da1d6abd
Antoine Thiberville <antoinethib@hotmail.com>
2016-05-04 17:20:10 +0200

    Classes updated

--- app/src/main/java/ekylibre/api/Plants.java
+++ app/src/main/java/ekylibre/api/Plants.java
@@ -1,7 +1,71 @@
 package ekylibre.api;
 
+import android.graphics.drawable.shapes.Shape;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import ekylibre.exceptions.HTTPException;
+
 /**
  * Created by antoine on 22/04/16.
  */
+
 public class Plants {
+    int mId;
+    String mName;
+    String mVariety;
+    //shape
+    Boolean mActive;
+
+    public static List<Plants> all(Instance instance, JSONObject attributes) throws JSONException, IOException, HTTPException {
+        // JSONObject params = Instance.BundleToJSON(attributes);
+        JSONObject params = attributes;
+
+        JSONArray json = instance.getJSONArray("/api/v1/plants", params);
+
+        List<Plants> array = new ArrayList<>();
+
+        for(int i = 0 ; i < json.length() ; i++ ){
+
+            array.add(new Plants(json.getJSONObject(i)));
+        }
+
+        return array;
+    }
+
+    public Plants(JSONObject object) throws JSONException{
+
+        Log.d("zero", "Object Plants : " + object.toString());
+
+        mId = object.getInt("id");
+        mName = object.getString("name");
+        mVariety =object.getString("variety_name");
+        //mShape = (float)object.getDouble("shape");
+        mActive = object.getBoolean("active");
+    }
+
+    public int getId() {
+        return mId;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public String getVariety() {
+        return mVariety;
+    }
+
+    public Boolean getActive() {
+        return mActive;
+    }
+
+
 }
--- app/src/main/java/ekylibre/api/PlantsCounting.java
+++ app/src/main/java/ekylibre/api/PlantsCounting.java
@@ -1,7 +1,45 @@
 package ekylibre.api;
 
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import ekylibre.exceptions.HTTPException;
+
 /**
  * Created by antoine on 22/04/16.
  */
 public class PlantsCounting {
+    int mId;
+    String mRead_at;
+    public static List<PlantsCounting> all(Instance instance, JSONObject attributes) throws JSONException, IOException, HTTPException {
+        // JSONObject params = Instance.BundleToJSON(attributes);
+        JSONObject params = attributes;
+
+        JSONArray json = instance.getJSONArray("/api/v1/plants_counting", params);
+
+        List<PlantsCounting> array = new ArrayList<>();
+
+        for(int i = 0 ; i < json.length() ; i++ ){
+
+            array.add(new PlantsCounting(json.getJSONObject(i)));
+        }
+
+        return array;
+    }
+
+    public PlantsCounting(JSONObject object) throws JSONException{
+
+        Log.d("zero", "Object Plants : " + object.toString());
+
+        mId = object.getInt("id");
+        mRead_at = object.getString("read_at");
+    }
 }
