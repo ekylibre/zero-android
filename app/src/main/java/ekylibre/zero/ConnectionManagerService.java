77e365f48f727d96ff1506c88f9880328980f432
PierreBougon <pierre.bougon@epitech.eu>
2016-07-06 15:20:24 +0200

    connectionManager needs to access to maccount to run

--- app/src/main/java/ekylibre/zero/ConnectionManagerService.java
+++ app/src/main/java/ekylibre/zero/ConnectionManagerService.java
@@ -1,9 +1,16 @@
 package ekylibre.zero;
 
+import android.accounts.Account;
 import android.app.Service;
+import android.content.ContentResolver;
+import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.IBinder;
 import android.util.Log;
+import android.widget.Toast;
 
 /**
  * Created by PierreBougon on 7/6/16.
@@ -12,17 +19,35 @@ import android.util.Log;
 
 public class ConnectionManagerService extends Service
 {
-    public ConnectivityManager connectivityManager;
-    public NetworkInfo networkInfo;
+    private ConnectivityManager     connectivityManager;
+    private NetworkInfo             networkInfo;
+    private Handler                 handler;
+    private Account                 mAccount;
 
-    public ConnectionManagerService ()
+    @Override
+    public void onCreate ()
     {
+        final int       hDelay;
+        MenuActivity    menu = new MenuActivity();
+
+        // Need to change this to get the actual account
+        mAccount = menu.getAccount();
         connectivityManager = (ConnectivityManager)getSystemService(CONNECTIVITY_SERVICE);
         networkInfo = connectivityManager.getActiveNetworkInfo();
-        return (this);
+        handler = new Handler();
+        hDelay = 10000;
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run()
+            {
+                if (try_connection())
+                    syncAll();
+                handler.postDelayed(this, hDelay);
+            }
+        }, hDelay);
     }
 
-    public boolean  try_connection()
+    private boolean  try_connection()
     {
         boolean     wifi;
 
@@ -36,5 +61,22 @@ public class ConnectionManagerService extends Service
                 return (false);
         }
         return (false);
+    }
+
+    private void syncAll()
+    {
+        Log.d("zero", "syncData: " + mAccount.toString() + ", " + ZeroContract.AUTHORITY);
+        Bundle extras = new Bundle();
+        extras.putBoolean(ContentResolver.SYNC_EXTRAS_MANUAL, true);
+        extras.putBoolean(ContentResolver.SYNC_EXTRAS_EXPEDITED, true);
+
+        ContentResolver.requestSync(mAccount, ZeroContract.AUTHORITY, extras);
+        Toast toast = Toast.makeText(getApplicationContext(), R.string.data_synced, Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public IBinder onBind(Intent intent)
+    {
+        return (null);
     }
 }
