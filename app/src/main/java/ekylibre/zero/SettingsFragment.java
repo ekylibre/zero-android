e4f3aaf7a8434c684a1532c646f79532404c6149
PierreBougon <pierre.bougon@epitech.eu>
2016-07-25 17:02:19 +0200

    clean sync adapter

--- app/src/main/java/ekylibre/zero/SettingsFragment.java
+++ app/src/main/java/ekylibre/zero/SettingsFragment.java
@@ -4,9 +4,11 @@ import android.preference.PreferenceFragment;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 
-public class SettingsFragment extends PreferenceFragment {
+public class SettingsFragment extends PreferenceFragment
+{
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
 
         // Load the preferences from an XML resource
