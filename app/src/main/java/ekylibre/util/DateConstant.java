f6c8337ebc06dc24a219ddc6ce9e55305d79974b
PierreBougon <pierre.bougon@epitech.eu>
2016-09-01 13:58:04 +0200

    date constant class permit to get date format

---
+++ app/src/main/java/ekylibre/util/DateConstant.java
@@ -0,0 +1,10 @@
+package ekylibre.util;
+
+/**************************************
+ * Created by pierre on 9/1/16.      *
+ * ekylibre.util for zero-android    *
+ *************************************/
+public final class DateConstant
+{
+    public static final String ISO_8601 = "yyyy-MM-dd'T'HH:mm:ss.SSSZ";
+}
