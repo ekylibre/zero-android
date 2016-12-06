144d01ec37de8019a04aee7b865dfa2c9ad917d5
PierreBougon <pierre.bougon@epitech.eu>
2016-12-06 16:47:31 +0100

    some clean on IssueActivity

--- app/src/main/java/ekylibre/zero/IssueActivity.java
+++ app/src/main/java/ekylibre/zero/IssueActivity.java
@@ -274,7 +274,6 @@ public class IssueActivity extends UpdatableActivity
         int duration = Toast.LENGTH_SHORT;
         Toast toast = Toast.makeText(context, text, duration);
         toast.show();
-
         //http://developer.android.com/guide/topics/providers/content-provider-basics.html
 
         // Defines an object to contain the new values to insert
