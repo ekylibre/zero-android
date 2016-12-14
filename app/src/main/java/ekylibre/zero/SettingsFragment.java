891c1933f80000772910b9fd715eafc7951afa0c
PierreBougon <pierre.bougon@epitech.eu>
2016-12-14 17:56:38 +0100

    update Settings

--- app/src/main/java/ekylibre/zero/SettingsFragment.java
+++ app/src/main/java/ekylibre/zero/SettingsFragment.java
@@ -1,8 +1,16 @@
 package ekylibre.zero;
 
+import android.app.Activity;
+import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class SettingsFragment extends PreferenceFragment
 {
@@ -13,7 +21,5 @@ public class SettingsFragment extends PreferenceFragment
 
         // Load the preferences from an XML resource
         addPreferencesFromResource(R.xml.preferences);
-
     }
-
 }
