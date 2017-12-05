c30f4314006f498165a867f3032e19f809f6bd6a
Rémi de Chazelles <remidechazelles@gmail.com>
2017-12-05 15:59:48 +0100

    Upgrades projet for Android Studio 3

--- app/src/main/java/ekylibre/zero/SettingsFragment.java
+++ app/src/main/java/ekylibre/zero/SettingsFragment.java
@@ -1,16 +1,8 @@
 package ekylibre.zero;
 
-import android.app.Activity;
-import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+
 
 public class SettingsFragment extends PreferenceFragment
 {
