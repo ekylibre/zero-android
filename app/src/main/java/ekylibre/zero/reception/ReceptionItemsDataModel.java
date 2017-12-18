d1bba2c7246753851a57133723c50f3a74b180ca
Pierreptcrb <pierre.putcrabey@gmail.com>
2017-12-18 16:26:16 +0100

    creation of ReceptionItemsDataModel

---
+++ app/src/main/java/ekylibre/zero/reception/ReceptionItemsDataModel.java
@@ -0,0 +1,49 @@
+package ekylibre.zero.reception;
+
+/**
+ * Created by Pierre on 18/12/2017.
+ */
+
+public class ReceptionItemsDataModel {
+    private int id;
+    private int fk_reception ;
+    private int fk_article;
+    private double quantity ;
+    private int ek_id;
+
+    public ReceptionItemsDataModel(int id, ReceptionDataModel fk_reception, ArticleDataModel fk_article, double quantity, int ek_id) {
+        this.id = id;
+        this.fk_reception = ReceptionDataModel.getId ;
+        this.fk_article = ArticleDataModel.getId;
+        this.quantity = quantity ;
+        this.ek_id = ek_id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getFk_reception() {
+        return fk_reception;
+    }
+
+    public int getFk_article() {
+        return fk_article;
+    }
+
+    public double getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(double quantity) {
+        this.quantity = quantity;
+    }
+
+    public int getEk_id() {
+        return ek_id;
+    }
+
+    public void setEk_id(int ek_id) {
+        this.ek_id = ek_id;
+    }
+}
