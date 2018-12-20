9b1b8f13235f374f8572852836c2f2c6c9ceaf01
Julien Lorriette <julien.lorriette@agro-bordeaux.fr>
2018-12-20 11:44:56 +0100

    implemented abstract "SelectProductCategoryFragment.OnFragmentInteractionListener" method

--- app/src/main/java/ekylibre/zero/inventory/NewInventory.java
+++ app/src/main/java/ekylibre/zero/inventory/NewInventory.java
@@ -9,6 +9,7 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Switch;
 import android.widget.TextView;
 
@@ -28,23 +29,27 @@ public class NewInventory extends AppCompatActivity implements SelectProductCate
 
 
 
-//todo: Faire une méthode pour enregistrer automatiquement la denrière date d'inventaire dans la zone
+//todo: Faire une méthode pour enregistrer automatiquement la dernière date d'inventaire dans la zone
    @Override
     protected void onCreate(Bundle savedInstanceState) {
        super.onCreate(savedInstanceState);
        setContentView(R.layout.activity_new_current_inventory);
 
+
+
+
+
        class MyButtonClickListener implements View.OnClickListener {
            @Override
            public void onClick(View _buttonView) {
-               if (_buttonView.getId() == R.id.addproduct) {
-                   Log.i("mytag", "categorychoice");
-                   Intent intent=new Intent(this,thirdactivity.class);
-                   startActivity(intent);
+//               if (_buttonView.getId() == R.id.choose_category) {
+//                   Log.i("mytag", "categorychoice");
+//                   Intent intent = new Intent(this, thirdactivity.class);
+//                   startActivity(intent);
+//               }
+               if (_buttonView.getId() == R.id.validate_button) {
 
-
-                   }
-
+               }
            }
 
 
@@ -58,10 +63,10 @@ public class NewInventory extends AppCompatActivity implements SelectProductCate
 
            }
        });
-
-
-
    }
+
+
+
 
     private FragmentTransaction getFragmentTransaction (){
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
@@ -72,6 +77,20 @@ public class NewInventory extends AppCompatActivity implements SelectProductCate
         ft.addToBackStack(null);
         return ft;
     }
+
+    @Override
+    public void onFragmentInteraction(ItemCategoryInventory zone) {
+
+    }
+    /*@Override
+    public void onFragmentInteraction(ItemZoneInventory zone) {
+        Log.i("MyTag", "click zone"+zone.zone);
+
+
+
+        mAdapter.notifyDataSetChanged();
+        selectCategoryDialogFragment.dismiss();
+    }*/
 
 }
 
