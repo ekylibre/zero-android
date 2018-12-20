b8c85f28ae6ba778a03511756f4e3deeb7362ec3
Diane <diane.d@laposte.net>
2018-12-20 10:17:04 +0100

    created a new class for product item + light id edits in layout product_item

---
+++ app/src/main/java/ekylibre/zero/inventory/ItemProductInventory.java
@@ -0,0 +1,35 @@
+package ekylibre.zero.inventory;
+
+
+public class ItemProductInventory {
+    public String productName;
+    public String productVariant;
+    public String productDate;
+    public String productQuantity;
+    public String productComment;
+    public byte[] productImage;
+
+    public ItemProductInventory(String productName, String productVariant, String productDate,
+                                String productQuantity, String productComment, byte[] productImage) {
+        this.productName = productName;
+        this.productVariant = productVariant;
+        this.productDate = productDate;
+        this.productQuantity = productQuantity;
+        this.productComment = productComment;
+        this.productImage = productImage;
+    }
+
+    /*
+    public ItemProductInventory(String zone) {
+        this.zone = zone;
+        this.dateInventory = null;
+        this.Icon_zone = null;
+    }
+
+
+    @Override
+    public String toString() {
+        return dateInventory + ',' + zone ;
+    }
+    */
+}
