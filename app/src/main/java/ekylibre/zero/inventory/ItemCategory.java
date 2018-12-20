9571d77c81f6e47e54e42fcb4dce572e7a4d1fa6
Galdriel BRIERE <galadriel.briere@gmail.com>
2018-12-20 14:50:41 +0100

    Add links to API to sync databases

---
+++ app/src/main/java/ekylibre/zero/inventory/ItemCategory.java
@@ -0,0 +1,74 @@
+package ekylibre.zero.inventory;
+
+
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import ekylibre.APICaller.Instance;
+import ekylibre.exceptions.HTTPException;
+import ekylibre.zero.BuildConfig;
+
+public class ItemCategory {
+    public String name;
+    int cat_id;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCat_id() {
+        return cat_id;
+    }
+
+    public void setCat_id(int cat_id) {
+        this.cat_id = cat_id;
+    }
+
+    public ItemCategory(int cat_id, String name) {
+        this.cat_id = cat_id;
+        this.name = name;
+    }
+
+    public static List<ItemCategory> all(Instance instance, String attributes) throws JSONException, IOException, HTTPException
+    {
+        // JSONObject params = Instance.BundleToJSON(attributes);
+        String params = attributes;
+        if (BuildConfig.DEBUG) Log.d("TAG", "Get JSONArray => /api/v1/category || params = " + params);
+        JSONArray json = instance.getJSONArray("/api/v1/category", params);
+        List<ItemCategory> array = new ArrayList<>();
+
+        for(int i = 0 ; i < json.length() ; i++ )
+        {
+            array.add(new ItemCategory(json.getJSONObject(i)));
+        }
+        return array;
+    }
+
+
+
+    public ItemCategory(JSONObject object) throws JSONException
+    {
+        if (BuildConfig.DEBUG) Log.d("zero", "Object Zone : " + object.toString());
+        cat_id = object.getInt("category_id");
+        name = object.getString("category_name");
+
+    }
+
+
+    @Override
+    public String toString() {
+        return cat_id + " : "+ name;
+    }
+}
+
