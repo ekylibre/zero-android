d527936af989ea77d338a737a75650dfb56eda25
Julien Lorriette <julien.lorriette@agro-bordeaux.fr>
2018-12-20 15:17:49 +0100

    Made view of activity 2 from menu

--- app/src/main/java/ekylibre/zero/home/MainActivity.java
+++ app/src/main/java/ekylibre/zero/home/MainActivity.java
@@ -37,6 +37,7 @@ import ekylibre.zero.SettingsActivity;
 import ekylibre.zero.account.AccountManagerActivity;
 import ekylibre.zero.intervention.InterventionActivity;
 import ekylibre.zero.inventory.InventoryActivity;
+import ekylibre.zero.inventory.NewInventory;
 
 /**************************************
  * Created by pierre on 7/12/16.      *
@@ -289,6 +290,13 @@ public class MainActivity extends UpdatableActivity
                 startActivity(intent);
                 break;
             }
+            case R.id.nav_testact :
+            {
+                Intent intent = new Intent(this, NewInventory.class);
+                startActivity(intent);
+                break;
+            }
+
             case R.id.nav_settings :
             {
                 Intent intent = new Intent(this, SettingsActivity.class);
